70bfaee4280d20fba6495709da62e0a661596bbc
yeeeeeeeeehor <88080877+yeeeeeeeeehor@users.noreply.github.com>
2022-06-10 18:26:49 +0300

    Update i.sh

--- subspace/i.sh
+++ subspace/i.sh
@@ -1,189 +1,86 @@
 #!/bin/bash
-
-function colors {
-  GREEN="\e[32m"
-  RED="\e[39m"
-  NORMAL="\e[0m"
+echo "========================================================================================="
+echo -e "\033[0;35m"
+echo -e "KKKKKKKKK    KKKKKKK               CCCCCCCCCCCCC         DDDDDDDDDDDDD"        
+echo -e "K:::::::K    K:::::K            CCC::::::::::::C         D::::::::::::DDD"     
+echo -e "K:::::::K    K:::::K          CC:::::::::::::::C         D:::::::::::::::DD"   
+echo -e "K:::::::K   K::::::K         C:::::CCCCCCCC::::C         DDD:::::DDDDD:::::D"  
+echo -e "KK::::::K  K:::::KKK        C:::::C       CCCCCC           D:::::D    D:::::D" 
+echo -e "  K:::::K K:::::K          C:::::C                         D:::::D     D:::::D"
+echo -e "  K::::::K:::::K           C:::::C                         D:::::D     D:::::D"
+echo -e "  K:::::::::::K            C:::::C                         D:::::D     D:::::D"
+echo -e "  K:::::::::::K            C:::::C                         D:::::D     D:::::D"
+echo -e "  K::::::K:::::K           C:::::C                         D:::::D     D:::::D"
+echo -e "  K:::::K K:::::K          C:::::C                         D:::::D     D:::::D"
+echo -e "KK::::::K  K:::::KKK        C:::::C       CCCCCC           D:::::D    D:::::D" 
+echo -e "K:::::::K   K::::::K         C:::::CCCCCCCC::::C         DDD:::::DDDDD:::::D"  
+echo -e "K:::::::K    K:::::K          CC:::::::::::::::C         D:::::::::::::::DD"   
+echo -e "K:::::::K    K:::::K            CCC::::::::::::C         D::::::::::::DDD"     
+echo -e "KKKKKKKKK    KKKKKKK               CCCCCCCCCCCCC         DDDDDDDDDDDDD"  
+echo -e "\e[0m"
+echo "========================================================================================="
+sleep 2
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
 }
+if exists curl; then
+	echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
 
-function logo {
-  curl -s https://raw.githubusercontent.com/yeeeeeeeeehor/Node/main/KCD.sh | bash
-}
 
-function line_1 {
-  echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}"
-}
+cd $HOME
+rm -rf subspace*
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-node-ubuntu-x86_64-gemini-1a-2022-may-31
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1a-2022-may-31/subspace-farmer-ubuntu-x86_64-gemini-1a-2022-may-31
+chmod +x subspace*
+mv subspace* /usr/local/bin/
 
-function line_2 {
-  echo -e "${RED}##############################################################################${NORMAL}"
-}
+systemctl stop subspaced subspaced-farmer &>/dev/null
+rm -rf ~/.local/share/subspace*
 
-function install_tools {
-  sudo apt update && sudo apt install mc wget htop jq git -y
-}
+source ~/.bash_profile
+sleep 1
 
-function install_docker {
-  curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_docker.sh | bash
-}
+echo "[Unit]
+Description=Subspace Node
+After=network.target
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which subspace-node) --chain gemini-1 --execution wasm --pruning 1024 --keep-blocks 1024 --validator --name $SUBSPACE_NODENAME
+Restart=on-failure
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target" > $HOME/subspaced.service
 
-function install_ufw {
-  curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash
-}
 
-function read_nodename {
-  if [ ! $SUBSPACE_NODENAME ]; then
-  echo -e "Enter your node name(random name for telemetry)"
-  line_1
-  read SUBSPACE_NODENAME
-  fi
-}
+echo "[Unit]
+Description=Subspaced Farm
+After=network.target
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which subspace-farmer) farm --reward-address $SUBSPACE_WALLET --plot-size 40G
+Restart=on-failure
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target" > $HOME/subspaced-farmer.service
 
-function read_wallet {
-  if [ ! $WALLET_ADDRESS ]; then
-  echo -e "Enter your polkadot.js extension address"
-  line_1
-  read WALLET_ADDRESS
-  fi
-}
 
-# function source_git {
-#   git clone https://github.com/subspace/subspace
-#   cd $HOME/subspace
-#   git fetch
-#   git checkout gemini-1b-2022-june-05
-# }
-#
-# function build_image_node {
-#   cd $HOME/subspace
-#   docker build -t subspacelabs/subspace-node:gemini-1b-2022-june-05 -f $HOME/subspace/Dockerfile-node .
-# }
-#
-# function build_image_farmer {
-#   cd $HOME/subspace
-#   docker build -t subspacelabs/subspace-farmer:gemini-1b-2022-june-05 -f $HOME/subspace/Dockerfile-farmer .
-# }
-
-function eof_docker_compose {
-  mkdir -p $HOME/subspace_docker/
-  sudo tee <<EOF >/dev/null $HOME/subspace_docker/docker-compose.yml
-  version: "3.7"
-  services:
-    node:
-      image: ghcr.io/subspace/node:gemini-1b-2022-june-05
-      volumes:
-        - node-data:/var/subspace:rw
-      ports:
-        - "0.0.0.0:30333:30333"
-      restart: unless-stopped
-      command: [
-        "--chain", "gemini-1",
-        "--base-path", "/var/subspace",
-        "--execution", "wasm",
-        "--pruning", "1024",
-        "--keep-blocks", "1024",
-        "--port", "30333",
-        "--rpc-cors", "all",
-        "--rpc-methods", "safe",
-        "--unsafe-ws-external",
-        "--validator",
-        "--name", "$SUBSPACE_NODENAME",
-        "--telemetry-url", "wss://telemetry.subspace.network/submit 0",
-        "--telemetry-url", "wss://telemetry.postcapitalist.io/submit 0",
-        "--reserved-nodes", "/dns/bootstrap-0.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWF9CgB8bDvWCvzPPZrWG3awjhS7gPFu7MzNPkF9F9xWwc",
-        "--reserved-nodes", "/dns/bootstrap-1.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWLrpSArNaZ3Hvs4mABwYGDY1Rf2bqiNTqUzLm7koxedQQ",
-        "--reserved-nodes", "/dns/bootstrap-2.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWNN5uuzPtDNtWoLU28ZDCQP7HTdRjyWbNYo5EA6fZDAMD",
-        "--reserved-nodes", "/dns/bootstrap-3.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWM47uyGtvbUFt5tmWdFezNQjwbYZmWE19RpWhXgRzuEqh",
-        "--reserved-nodes", "/dns/bootstrap-4.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWNMEKxFZm9mbwPXfQ3LQaUgin9JckCq7TJdLS2UnH6E7z",
-        "--reserved-nodes", "/dns/bootstrap-5.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWFfEtDmpb8BWKXoEAgxkKAMfxU2yGDq8nK87MqnHvXsok",
-        "--reserved-nodes", "/dns/bootstrap-6.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWHSeob6t43ukWAGnkTcQEoRaFSUWphGDCKF1uefG2UGDh",
-        "--reserved-nodes", "/dns/bootstrap-7.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWKwrGSmaGJBD29agJGC3MWiA7NZt34Vd98f6VYgRbV8hH",
-        "--reserved-nodes", "/dns/bootstrap-8.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWCXFrzVGtAzrTUc4y7jyyvhCcNTAcm18Zj7UN46whZ5Bm",
-        "--reserved-nodes", "/dns/bootstrap-9.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWNGxWQ4sajzW1akPRZxjYM5TszRtsCnEiLhpsGrsHrFC6",
-        "--reserved-nodes", "/dns/bootstrap-10.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWNGf1qr5411JwPHgwqftjEL6RgFRUEFnsJpTMx6zKEdWn",
-        "--reserved-nodes", "/dns/bootstrap-11.gemini-1b.subspace.network/tcp/30333/p2p/12D3KooWM7Qe4rVfzUAMucb5GTs3m4ts5ZrFg83LZnLhRCjmYEJK"#,
-        # "--reserved-nodes", "/ip4/65.21.227.181/tcp/30333/p2p/12D3KooWNUWw6VAwey273vXgYxwr7fYRm2MMFWmSXrctA7ns1Vvb",
-        # "--reserved-only"
-      ]
-      healthcheck:
-        timeout: 5s
-        interval: 30s
-        retries: 5
-    farmer:
-      depends_on:
-        - node
-      image: ghcr.io/subspace/farmer:gemini-1b-2022-june-05
-      volumes:
-        - farmer-data:/var/subspace:rw
-      restart: unless-stopped
-      command: [
-        "--base-path", "/var/subspace",
-        "farm",
-        "--node-rpc-url", "ws://node:9944",
-        "--ws-server-listen-addr", "0.0.0.0:9955",
-        "--reward-address", "$WALLET_ADDRESS",
-        "--plot-size", "100G"
-      ]
-  volumes:
-    node-data:
-    farmer-data:
-EOF
-}
-
-function docker_compose_up {
-  docker-compose -f $HOME/subspace_docker/docker-compose.yml up -d
-}
-
-function echo_info {
-  echo -e "${GREEN}Для остановки ноды и фармера subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml down \n ${NORMAL}"
-  echo -e "${GREEN}Для запуска ноды и фармера subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml up -d \n ${NORMAL}"
-  echo -e "${GREEN}Для перезагрузки ноды subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml restart node \n ${NORMAL}"
-  echo -e "${GREEN}Для перезагрузки фармера subspace: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml restart farmer \n ${NORMAL}"
-  echo -e "${GREEN}Для проверки логов ноды выполняем команду: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml logs -f --tail=100 node \n ${NORMAL}"
-  echo -e "${GREEN}Для проверки логов фармера выполняем команду: ${NORMAL}"
-  echo -e "${RED}   docker-compose -f $HOME/subspace_docker/docker-compose.yml logs -f --tail=100 farmer \n ${NORMAL}"
-}
-
-function delete_old {
-  docker-compose -f $HOME/subspace_docker/docker-compose.yml down &>/dev/null
-  docker volume rm subspace_docker_subspace-farmer subspace_docker_subspace-node &>/dev/null
-}
-
-colors
-line_1
-logo
-line_2
-read_nodename
-line_2
-read_wallet
-line_2
-echo -e "Установка tools, ufw, docker"
-line_1
-install_tools
-install_ufw
-install_docker
-delete_old
-line_1
-# echo -e "Скачиваем репозиторий"
-# source_git
-# line_1
-# echo -e "Билдим образ ноды"
-# build_image_node
-# line_1
-# echo -e "Билдим образ фармера"
-# build_image_farmer
-# line_1
-echo -e "Создаем docker-compose файл"
-line_1
-eof_docker_compose
-line_1
-echo -e "Запускаем docker контейнеры для node and farmer Subspace"
-line_1
-docker_compose_up
-line_2
-echo_info
-line_2
+mv $HOME/subspaced* /etc/systemd/system/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable subspaced subspaced-farmer
+sudo systemctl restart subspaced
+sleep 10
+sudo systemctl restart subspaced-farmer
+echo '=============== УСТАНОВКА ЗАВЕРШЕНА ==================='
+echo -e 'Проверка логов ноды: \e[1m\e[32mjournalctl -u subspaced -f -o cat \e[0m'
+echo -e 'Проверка логов фармера: \e[1m\e[32mdocker logs subspace-farmer-1 \e[0m'
