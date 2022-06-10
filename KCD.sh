ee19f2d8f9dd4346c2f3901d4653058d1a6bb1e4
yeeeeeeeeehor <88080877+yeeeeeeeeehor@users.noreply.github.com>
2022-06-10 17:26:38 +0300

    Create KCD.sh

---
+++ KCD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "KKKKKKKKK    KKKKKKK               CCCCCCCCCCCCC         DDDDDDDDDDDDD"        
+echo -e "K:::::::K    K:::::K            CCC::::::::::::C         D::::::::::::DDD"     
+echo -e "K:::::::K    K:::::K          CC:::::::::::::::C         D:::::::::::::::DD"   
+echo -e "K:::::::K   K::::::K         C:::::CCCCCCCC::::C         DDD:::::DDDDD:::::D"  
+echo -e "KK::::::K  K:::::KKK        C:::::C       CCCCCC           D:::::D    D:::::D" 
+echo -e "  K:::::K K:::::K          C:::::C                         D:::::D     D:::::D"
+echo -e "  K::::::K:::::K           C:::::C                         D:::::D     D:::::D"
+echo -e "  K:::::::::::K            C:::::C                         D:::::D     D:::::D"
+echo -e "  K:::::::::::K            C:::::C                         D:::::D     D:::::D"
+echo -e "  K::::::K:::::K           C:::::C                         D:::::D     D:::::D"
+echo -e "  K:::::K K:::::K          C:::::C                         D:::::D     D:::::D"
+echo -e "KK::::::K  K:::::KKK        C:::::C       CCCCCC           D:::::D    D:::::D" 
+echo -e "K:::::::K   K::::::K         C:::::CCCCCCCC::::C         DDD:::::DDDDD:::::D"  
+echo -e "K:::::::K    K:::::K          CC:::::::::::::::C         D:::::::::::::::DD"   
+echo -e "K:::::::K    K:::::K            CCC::::::::::::C         D::::::::::::DDD"     
+echo -e "KKKKKKKKK    KKKKKKK               CCCCCCCCCCCCC         DDDDDDDDDDDDD" 
